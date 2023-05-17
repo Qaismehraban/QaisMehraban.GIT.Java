2dee93beeef44042b7f5b243c88cf5daa6580deb
= <mehraban.qais1@gmail.com>
2023-05-17 00:40:41 -0700

    Adding class Hello World for GIT practice

---
+++ src/GitForClassPractice/addingRepository.java
@@ -0,0 +1,8 @@
+package GitForClassPractice;
+
+public class addingRepository {
+	public static void main(String[] args) {
+		System.out.println("Here is the information");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module QaisMehraban.GIT.Java {
+}
